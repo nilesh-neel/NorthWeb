2730b1a5c970d8c81d1e76dfacf41fde32b2ef67
Nilesh More <nilesh.more02@yahoo.com>
2018-08-04 13:34:07 +0530

    Add project files.

---
+++ NorthWeb/App_Start/BundleConfig.cs
@@ -0,0 +1,40 @@
+﻿using System.Web;
+using System.Web.Optimization;
+
+namespace NorthWeb
+{
+    public class BundleConfig
+    {
+        // For more information on bundling, visit https://go.microsoft.com/fwlink/?LinkId=301862
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+                        "~/Scripts/jquery-{version}.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
+                        "~/Scripts/jquery.validate*"));
+
+            // Use the development version of Modernizr to develop with and learn from. Then, when you're
+            // ready for production, use the build tool at https://modernizr.com to pick only the tests you need.
+            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
+                        "~/Scripts/modernizr-*"));
+
+            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
+                      "~/Scripts/bootstrap.js",
+                       "~/Scripts/jquery.navgoco.min.js",
+                       "~/Scripts/jquery.fullscreen-min.js"
+                       ));
+
+            bundles.Add(new ScriptBundle("~/bundles/appScripts").Include(
+                "~/Scripts/appScripts/app.js"
+                ));
+
+            bundles.Add(new StyleBundle("~/Content/css").Include(
+                      "~/Content/bootstrap.min.css",
+                      "~/Content/font-awesome.min.css",
+                      "~/Content/simple-line-icons.css",
+                      "~/Content/animate.css",
+                      "~/Content/main.css"));
+        }
+    }
+}
---
+++ NorthWeb/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="NorthWeb.MvcApplication" Language="C#" %>
